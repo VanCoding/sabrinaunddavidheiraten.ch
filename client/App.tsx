37f05d9b2ee7af9989f0f1203c8bb9f2cda48886
Patrik Stutz <patrik.stutz@gmail.com>
2020-04-18 10:51:02 +0200

    changed date

--- client/App.tsx
+++ client/App.tsx
@@ -1,43 +1,50 @@
-import React, { Children } from "react"
-import styled, {StyledFunction} from "styled-components"
-import {Container,Paragraph} from "./components"
-import Menu from "./Menu"
-import Infos from "./Infos"
-import Signup from "./Signup"
-import Ablauf from "./Ablauf"
-import Location from "./Location"
-import Kapelle from "./Kapelle"
-import Playlist from "./Playlist"
-import Fotos from "./Fotos"
-import {Countdown,TargetDate} from "./Countdown"
+import React, { Children } from "react";
+import styled, { StyledFunction } from "styled-components";
+import { Container, Paragraph } from "./components";
+import Menu from "./Menu";
+import Infos from "./Infos";
+import Signup from "./Signup";
+import Ablauf from "./Ablauf";
+import Location from "./Location";
+import Kapelle from "./Kapelle";
+import Playlist from "./Playlist";
+import Fotos from "./Fotos";
+import { Countdown, TargetDate } from "./Countdown";
 
 //@ts-ignore
 let config = window.config as {
-	googleKey: string,
-	playlistId: string
-}
+	googleKey: string;
+	playlistId: string;
+};
 
-const App = styled.div``
-const target = new Date(2020,4,16).getTime()
+const App = styled.div``;
+const target = new Date(2021, 4, 8, 12, 15).getTime();
 
-export default ()=>(
+export default () => (
 	<App>
 		<Container>
-			<div style={{display:"flex",flexDirection:"row",alignItems:"center",paddingBottom:"30px",paddingTop:"30px",justifyContent:"space-around"}}>
-				<TargetDate target={target}/>
-				<div style={{fontSize:"40px",color:"white"}}>❤</div>
-				<Countdown target={target}/>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+					paddingBottom: "30px",
+					paddingTop: "30px",
+					justifyContent: "space-around",
+				}}
+			>
+				<TargetDate target={target} />
+				<div style={{ fontSize: "40px", color: "white" }}>❤</div>
+				<Countdown target={target} />
 			</div>
-			<Menu/>
-			<Infos/>
-			<Ablauf/>
-			<Location googleKey={config.googleKey}/>
-			<Kapelle googleKey={config.googleKey}/>
-			<Signup/>
-			<Playlist playlistId={config.playlistId}/>
-			<Fotos/>
+			<Menu />
+			<Infos />
+			<Ablauf />
+			<Location googleKey={config.googleKey} />
+			<Kapelle googleKey={config.googleKey} />
+			<Signup />
+			<Playlist playlistId={config.playlistId} />
+			<Fotos />
 		</Container>
 	</App>
-)
-
-
+);
